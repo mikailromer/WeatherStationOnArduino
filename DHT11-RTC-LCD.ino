2a463a6cba5d1b6e6c3ae59666c4f5dbe2ec9779
Michal Camacho <mcamromer@gmail.com>
2019-02-10 23:09:01 +0100

    Ploting data from wheather station.

--- DHT11-RTC-LCD.ino
+++ DHT11-RTC-LCD.ino
@@ -4,10 +4,10 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#include <stdio.h>
+
 
 #define DHTPIN 8     
-#define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT11     
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
@@ -15,7 +15,7 @@ uint8_t counter=0;
 float temperature,humidity;
 
 DS3231 clock;
-RTCDateTime dt;
+RTCDateTime date;
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
 
@@ -32,33 +32,9 @@ void setup() {
   //Settings for DHT11 Temperature & Humidity Sensor
   // Initialize device.
   dht.begin();
-  //Serial.println(F("DHT11 Unified Sensor Example"));
-  // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
-  /*
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-  */
-  // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
-  /*
-  Serial.println(F("Humidity Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-  */
   // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
 
@@ -67,13 +43,13 @@ void setup() {
   lcd.begin(16,2);
   lcd.setCursor(0,0);
   lcd.print("T: ");
-  lcd.print(dt.hour);
+  lcd.print(date.hour);
   lcd.print(":");
-  lcd.print(dt.minute);
+  lcd.print(date.minute);
   lcd.print(":");
-  lcd.print(dt.second);
+  lcd.print(date.second);
   lcd.setCursor(0,1);
-  lcd.print(dt.hour);
+  lcd.print(date.hour);
  
 
 }
@@ -84,57 +60,33 @@ void loop() {
   // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    //Serial.println(F("Error reading temperature!"));
-  }
-  else {
-  //  Serial.print(F("Temperature: "));
-    //Serial.print(event.temperature);
-    temperature=event.temperature;
-    Serial.print(temperature);
-    Serial.print(",");
-   // Serial.println(F("°C"));
-  }
+  temperature=event.temperature;
+  Serial.print(temperature);
+  Serial.print(",");
   // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-   // Serial.println(F("Error reading humidity!"));
-  }
-  else {
-   // Serial.print(F("Humidity: "));
-    humidity=event.relative_humidity;
-    Serial.println(humidity);
-    //Serial.println(F("%"));
-  }
-  
-
-  
+  humidity=event.relative_humidity;
+  Serial.println(humidity);
   // Time reading
-  dt = clock.getDateTime();
-  /*
-  Serial.print("Raw data: ");
-  Serial.print(dt.year);   Serial.print("-");
-  Serial.print(dt.month);  Serial.print("-");
-  Serial.print(dt.day);    Serial.print(" ");
-  Serial.print(dt.hour);   Serial.print(":");
-  Serial.print(dt.minute); Serial.print(":");
-  Serial.print(dt.second); Serial.println("");
-  */
+  date = clock.getDateTime();
+
+  //LCD displayer time typing
+  //Time is actualized once per second
   lcd.setCursor(0,0);
   lcd.print("Time: ");
-  lcd.print(dt.hour);
+  lcd.print(date.hour);
   lcd.print(":");
-  lcd.print(dt.minute);
+  lcd.print(date.minute);
   lcd.print(":");
-  lcd.print(dt.second);
+  lcd.print(date.second);
   
   if(counter==0)
   {
+    //LCD displayer temperature and relative humidity typing
+    //Temperature and humidity are actualized once per 10 seconds
     lcd.setCursor(0,1);
     if (!(isnan(event.temperature)) && !(isnan(event.temperature))) {
-      char temp_string[4], hum_string[4];
       lcd.print("T:");
-      //Serial.println(temperature);
       lcd.print(temperature);
       lcd.print("C");
       lcd.print(" H:");
@@ -146,12 +98,8 @@ void loop() {
   counter=counter+1;
   if(counter==10)
   {
+    //After each 10 seconds LCD screen is cleaned.
     counter=0;
     lcd.clear();
   }
- 
-  
- 
-  //delay(1000);
-
 }
--- PlotDataFromSerial.py
+++ PlotDataFromSerial.py
@@ -1,5 +1,4 @@
 import serial
-import numpy
 import matplotlib.pyplot as plt
 from drawnow import *
 
@@ -8,12 +7,14 @@ humidityData=[]
 
 def createDataPlot():
     plt.grid(True)
-    plt.title('Temperature data from DHT11')
+    plt.title('Temperature & Relative Humidity data from DHT11')
+    #Settings for temperature graph
     plt.xlabel('Time [s]')
     plt.ylabel('Temperature [C]')
     plt.ylim(0, 50)
     plt.plot(temperatureData, 'ro-', label='Degrees C')
     plt.legend(loc='upper left')
+    #Settings for the second relative humidity graph
     plt2 = plt.twinx()
     plt.xlabel('Time [s]')
     plt.ylabel('Relative hummidity [%]')
@@ -22,24 +23,28 @@ def createDataPlot():
     plt2.ticklabel_format(useOffset=False)
     plt2.legend(loc='upper right')
 
-
-arduinoData = serial.Serial('com4', 9600)
-cnt=0
+#Initialization of serial port
+arduinoData = serial.Serial('com5', 9600)
+#Turn the interactive mode on.
 plt.ion()
 
 if __name__ == '__main__':
     while True:
+        #Get the number of bytes in the input buffer
         while (arduinoData.inWaiting() == 0):
             pass
-        arduinoString = arduinoData.readline()  # read the line of text from the serial port
+        arduinoString = arduinoData.readline()
+        # read the line of text from the serial port
         arduinoString=(arduinoString.decode('utf-8')).rstrip()
-        dataArray = arduinoString.split(',')  # Split it into an array called dataArray
-        temp = float(dataArray[0])  # Convert first element to floating number and put in temp
+        # Split it into an array called dataArray
+        dataArray = arduinoString.split(',')
+        temp = float(dataArray[0])
+        # Convert first element to floating number and put in temp
         hum=float(dataArray[1])
+        # Convert second element to floating number and put in hum
         temperatureData.append(temp)
         humidityData.append(hum)
+        #Plot drawing
         drawnow(createDataPlot)
         plt.pause(.000001)
-        cnt = cnt + 1
-        if (cnt > 50):
-            humidityData.pop(0)
+
